f72cd809c6e63b325af38b00e9c07da5d7c81b76
skgrange <s.k.grange@gmail.com>
2019-04-14 20:21:15 +0100

    :sparkles: add httr to aqer_read_csv

--- R/aqer_read_csv.R
+++ R/aqer_read_csv.R
@@ -4,12 +4,17 @@
 #' used other situations other than just reading many files, such as exporting 
 #' data in parallel. 
 #' 
-#' @param file Vector of file names to read.
+#' @param file Vector of file names to read or a character vector to be parsed. 
 #' 
 #' @param encoding Encoding of \code{file}. 
 #' 
 #' @param as_smonitor Should the return be formatted for use within the 
 #' \code{smonitor} framework? 
+#' 
+#' @param timeout When file is a URL, how many seconds should be used as a 
+#' timeout?
+#' 
+#' @param warn Should the function raise warnings? 
 #' 
 #' @param verbose Should the function give messages? 
 #' 
@@ -30,7 +35,7 @@
 #' 
 #' @export
 aqer_read_csv <- function(file, encoding = "UCS-2LE", as_smonitor = FALSE,
-                          verbose = FALSE) {
+                          timeout = 30, warn = TRUE, verbose = FALSE) {
   
   # Vectorise function
   purrr::map_dfr(
@@ -38,6 +43,8 @@ aqer_read_csv <- function(file, encoding = "UCS-2LE", as_smonitor = FALSE,
     aqer_read_csv_worker,
     encoding = encoding,
     as_smonitor = as_smonitor,
+    timeout = timeout,
+    warn = warn,
     verbose = verbose
   )
   
@@ -46,16 +53,36 @@ aqer_read_csv <- function(file, encoding = "UCS-2LE", as_smonitor = FALSE,
 
 #' @rdname aqer_read_csv
 #' @export
-aqer_read_csv_worker <- function(file, encoding, as_smonitor, verbose) {
+aqer_read_csv_worker <- function(file, encoding, as_smonitor, timeout, warn, 
+                                 verbose) {
   
   # The message
-  if (verbose) message(date_message(), basename(file))
+  if (verbose) message(date_message(), file)
   
+  # If a url, use httr to get contents
+  if (stringr::str_detect(file[1], "^http:|^https:")) {
+    file <- get_url_contents(file, timeout = timeout)
+  }
+
   # Try to load data with readr first
   df <- aqer_read_csv_worker_readr_safe(file)
   
   # Try with base read.table and different encoding
-  if (nrow(df) == 0) df <- aqer_read_csv_worker_base(file, encoding)
+  if (nrow(df) == 0) {
+    
+    if (warn) {
+      warning(
+        date_message(), 
+        basename(file), 
+        " has an odd encoding...",
+        call. = FALSE,
+        immediate. = TRUE
+      )
+    }
+    
+    df <- aqer_read_csv_worker_base(file, encoding)
+    
+  }
   
   # Clean table for smonitor
   if (as_smonitor) {
@@ -148,5 +175,26 @@ aqer_read_csv_worker_base <- function(file, encoding) {
     as_tibble()
   
   return(df)
+  
+}
+
+
+get_url_contents <- function(url, timeout = 60) {
+  
+  # Get page
+  response <- httr::GET(url, httr::timeout(timeout))
+  
+  # Check response
+  httr::stop_for_status(response)
+  
+  # stringi::stri_enc_detect(httr::content(response, "raw"))
+  
+  # Decode content
+  x <- httr::content(response, "text", encoding = "UTF-8")
+  
+  # httr is not handling some special characters
+  if (is.na(x)) x <- stringi::stri_encode(response$content, "UTF-16")
+  
+  return(x)
   
 }
